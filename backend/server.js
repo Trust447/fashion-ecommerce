44edcdd3083f838090ba746dc96fbb98ca72c943
Trust Ihemebiri <isaactrust447@gmail.com>
2025-07-29 21:50:46 +0100

    created server and connected to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
+
+connectDB();
+
 
 app.get("/", (req,res)=>{
     res.send('Hello e-commerce API')
