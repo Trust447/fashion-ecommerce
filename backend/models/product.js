df9e41e8feac9895c4fb5a24dd68248946d6f1d1
Trust Ihemebiri <isaactrust447@gmail.com>
2025-07-30 19:01:40 +0100

    product model

---
+++ backend/models/product.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose"
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type : String,
+        required : [true, "Product name is required"]
+    },
+
+    description : {
+        type: String,
+        required : [true, "Description is required"]
+    },
+    price : {
+        type : Number,
+        required : [true, "Price is required"],
+        min : [0, "Price cannot be negative"],
+        default : 0
+    },
+    categogry : {
+        type : String,
+        required : [true, "Category is required"]
+    },
+    brand : {
+        type : String
+    },
+    countInStock : {
+        type : Number,
+        required : true,
+        min : [0, "Stock can't be negative"],
+        default : 0,
+    },
+    images : [
+        {
+            type : String
+        }
+    ],
+
+    timestamps : true
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
